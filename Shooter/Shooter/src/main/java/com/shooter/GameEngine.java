1f03d1db219ab971d09cfcdffaacbc53a8a47d94
pedrompg <pedrompg@gmail.com>
2013-12-17 21:59:19 +0100

    Fade animation between splash screen and main menu activity

---
+++ Shooter/Shooter/src/main/java/com/shooter/GameEngine.java
@@ -0,0 +1,5 @@
+package com.shooter;
+
+public class GameEngine {
+    public static final int GAME_THREAD_DELAY = 4000;
+}
--- Shooter/Shooter/src/main/java/com/shooter/MainMenuActivity.java
+++ Shooter/Shooter/src/main/java/com/shooter/MainMenuActivity.java
@@ -1,35 +1,24 @@
 package com.shooter;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 
+public class MainMenuActivity extends Activity {
 
-public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        new Handler().postDelayed(new Thread() {
-            @Override
-            public void run() {
-                Intent mainMenu = new Intent(MainActivity.this, MainMenuActivity.class);
-                MainActivity.this.startActivity(mainMenu);
-                MainActivity.this.finish();
-                overridePendingTransition(R.layout.fadein, R.layout.fadeout);
-            }
-        }, GameEngine.GAME_THREAD_DELAY);
+        setContentView(R.layout.activity_main_menu);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
@@ -39,6 +28,9 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
